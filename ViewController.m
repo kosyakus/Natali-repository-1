ad5a07c7cbff2f25b92161ec8cc046a535940cda
Unknown <kosyak_@mail.ru>
2017-05-03 21:11:09 +0300

    Added picture

---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HW1.2
+//
+//  Created by Admin on 03.05.17.
+//  Copyright © 2017 GB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW1.2
+//
+//  Created by Admin on 03.05.17.
+//  Copyright © 2017 GB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
